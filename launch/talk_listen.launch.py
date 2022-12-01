da4f556c42d875b3471c50824fcebc3605803716
Rikuho Takeda <s21c1074ho@s.chibakoudai.jp>
2022-12-01 14:40:30 +0900

    fix about launch

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
 
     talker = launch_ros.actions.Node(
             package='mypkg',
-            node_executable='talker',
+            executable='talker',
             )
     listener = launch_ros.actions.Node(
             package='mypkg',
